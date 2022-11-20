3ae4ac3579b6fd1b661b46482f6595ac4fa117f4
Christian Rullan <laptopchris01@gmail.com>
2022-11-20 15:25:58 -0500

    improved comment section

--- beatreal/src/app/comments/comments.component.ts
+++ beatreal/src/app/comments/comments.component.ts
@@ -9,8 +9,25 @@ export class CommentsComponent implements OnInit {
   @Output() viewCommentsEvent = new EventEmitter();
   constructor() {}
 
-  commentObj = { userId: 55, textContent: 'Hello' };
-  comments = [this.commentObj];
+  commentObj = { username: 'user5678355', textContent: 'Hello' };
+  commentObj2 = {
+    username: 'user123987',
+    textContent:
+      'adilsfjaljdshflakudshfluahdsfluhasdfkjadsfhlkjadshfluhasdflujahsdkljfnalsjdflajdfskajdshfkjhasdjkfhaskjdfhajksdfhkajdgshfkahgsdbfkjadgshfkjhaskdjghaksgfalsudjfhakdfhkadfshjh',
+  };
+  commentObj3 = {
+    username: 'user89273',
+    textContent:
+      'overflow overflow overflow overflow overflow overflow overflow overflow overflow overflow overflow overflowwwwww overflow',
+  };
+  comments = [
+    this.commentObj,
+    this.commentObj,
+    this.commentObj,
+    this.commentObj2,
+    this.commentObj3,
+    this.commentObj,
+  ];
 
   toggleComments() {
     this.viewCommentsEvent.emit();
