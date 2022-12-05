6ea7dd3e4a48d35d1d43c3f6051a8f0db466a56b
aidant <aidant@udel.edu>
2022-12-05 00:41:56 -0500

    finally got blob scroll working

--- beatreal/src/app/blobs/blobs.component.ts
+++ beatreal/src/app/blobs/blobs.component.ts
@@ -15,41 +15,52 @@ import {
 export class BlobsComponent implements OnInit {
   myLava: any;
   prevScrollY: number;
+  lavaHeight: number;
   constructor() {
     this.prevScrollY = window.pageYOffset;
+    this.lavaHeight = 0;
   }
   ngOnInit(): void {
     this.myLava = document.getElementById('lavaid');
+    this.lavaHeight = -this.myLava.offsetHeight * -0.3;
   }
 
-  @HostListener('window:scroll', []) onWindowScroll() {
-    const currScroll = window.pageYOffset ||
-    document.documentElement.scrollTop ||
-    document.body.scrollTop ||
-    0;
-    const scroll = currScroll - this.prevScrollY;
-    if (scroll > 0) {
-      if (this.myLava.style.top + )
+  @HostListener('window:mousewheel', ['$event'])
+  scroll(event: WheelEvent) {
+    // let scrollDist = 75;
+    // if (event.deltaY < 0) {
+    //   scrollDist *= -1;
+    // }
+    this.lavaHeight += event.deltaY * 0.2;
+    if (this.lavaHeight < 0) {
+      this.lavaHeight = 0;
+    } else if (this.lavaHeight > this.myLava.offsetHeight * 0.6) {
+      this.lavaHeight = this.myLava.offsetHeight * 0.6;
     }
-    const px = Math.round(
-      window.pageYOffset ||
-        document.documentElement.scrollTop ||
-        document.body.scrollTop ||
-        0
-    );
-    console.log(this.myLava.offsetHeight * 0.7);
-    console.log(px);
-    if (px > this.myLava.offsetHeight * 0.7) {
-      return;
-    }
-    this.myLava.style.top =
-      '-' +
-      Math.round(
-        window.pageYOffset ||
-          document.documentElement.scrollTop ||
-          document.body.scrollTop ||
-          0
-      ) +
-      'px';
+
+    const px = '-' + this.lavaHeight + 'px';
+
+    this.myLava.style.top = px;
   }
+
+  // @HostListener('window:scroll', []) onWindowScroll() {
+  //   console.log('do do do');
+  //   const currScroll =
+  //     window.pageYOffset ||
+  //     document.documentElement.scrollTop ||
+  //     document.body.scrollTop ||
+  //     0;
+  //   const scroll = currScroll - this.prevScrollY;
+  //   this.prevScrollY = currScroll;
+  //   this.lavaHeight += scroll;
+  //   if (this.lavaHeight < 0) {
+  //     this.lavaHeight = 0;
+  //   } else if (this.lavaHeight > this.myLava.offsetHeight * 0.6) {
+  //     this.lavaHeight = this.myLava.offsetHeight * 0.6;
+  //   }
+
+  //   const px = '-' + this.lavaHeight + 'px';
+
+  //   this.myLava.style.top = px;
+  // }
 }
