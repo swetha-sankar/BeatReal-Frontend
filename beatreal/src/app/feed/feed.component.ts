26de4abecd9ee7de465697084dd70b29c2a04387
ethanchang34 <ethanchang34@yahoo.com>
2022-12-06 13:47:25 -0500

    grabbed params and stored in sessionStorage

--- beatreal/src/app/feed/feed.component.ts
+++ beatreal/src/app/feed/feed.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
+import { ActivatedRoute } from '@angular/router';
+import * as internal from 'stream';
 
 @Component({
   selector: 'app-feed',
@@ -8,15 +10,38 @@ import { DialogComponent } from '../dialog/dialog.component';
   styleUrls: ['./feed.component.css'],
 })
 export class FeedComponent implements OnInit {
-  constructor(public dialog: MatDialog) {}
+  access_token!: string;
+  token_type!: string;
+  scope!: string;
+  expires_in!: number;
+  refresh_token!: string;
+
+  constructor(public dialog: MatDialog, private router: ActivatedRoute) {
+  }
 
   openDialog() {
     console.log("dialog open");
     this.dialog.open(DialogComponent);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.router.queryParams.subscribe(params => {
+      this.access_token = params['access_token'];
+      console.log('access_token:', this.access_token);
+      this.token_type = params['token_type'];
+      this.scope = params['scope'];
+      this.expires_in = params['expires_in'];
+      this.refresh_token = params['refresh_token'];
+      sessionStorage.setItem("access_token", this.access_token);
+      sessionStorage.setItem('token_type', this.token_type);
+      sessionStorage.setItem('scope', this.scope);
+      sessionStorage.setItem('expires_in', this.expires_in.toString());
+      sessionStorage.setItem('refresh_token', this.refresh_token);
+    })
+  }
 
+  // sendToComp = JSON.parse(`{"access_token":${this.access_token}, "token_type": ${this.token_type}, "scope": ${this.scope}, "expires_in": ${this.expires_in}, "refresh_token": ${this.refresh_token}}`);
+  
   route = 'feed';
 
   posts = [1, 2];
