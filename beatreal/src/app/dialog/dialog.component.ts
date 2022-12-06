37586d58b1657fe14fa7b95f14a870079b1de6b6
ethanchang34 <ethanchang34@yahoo.com>
2022-12-06 14:17:30 -0500

    can api call succesfully for default search

--- beatreal/src/app/dialog/dialog.component.ts
+++ beatreal/src/app/dialog/dialog.component.ts
@@ -8,29 +8,31 @@ import { HttpClient, HttpHeaders} from '@angular/common/http';
   styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent implements OnInit {
-data: any;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    console.log('sessionStorage stuffs', sessionStorage.getItem("access_token"));
   }
 
   // artistID="https://api.spotify.com/vi/search";
-  // headers= new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', ' ');
+  headers= new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', ' ');
 
 
   //Search
-  // async search() {
-  //   //GET request using search to get the Artist ID
-  //   let artistParameters = {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Authorization': 'Bearer ' + accessToken
-  //     }
-  //   }
 
-  //   let artistID = await fetch("https://api.spotify.com/vi/search", artistParameters);
-  // }
+  async search() {
+    //GET request using search to get the Artist ID
+    let artistParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('access_token')
+      }
+    }
+
+    let artistID = await fetch("https://api.spotify.com/v1/search?q=" + 'Taylor Swift' + '&type=artist', artistParameters)
+    .then(response => response.json()).then(data => console.log(data));
+  }
 
 }
