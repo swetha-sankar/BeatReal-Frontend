be4f20ed3b8e4d927c07fb852462b02a88059459
laurenbaron <lbaron@udel.edu>
2022-12-06 20:35:55 -0500

    start feed link to spotify song stuff

--- beatreal/src/app/post/post.component.ts
+++ beatreal/src/app/post/post.component.ts
@@ -28,41 +28,9 @@ export class PostComponent implements OnInit {
   }
 
   ngOnInit(): void {}
-
-  songUrl = 'https://api.spotify.com/v1/me/player/currently-playing';
-  headers = new HttpHeaders()
-    .set('Content-Type', 'application/json')
-    .set('Authorization', ' ');
-  //https://v2.angular.io/docs/ts/latest/guide/server-communication.html#:~:text=The%20Angular%20Http%20client%20communicates%20with%20the%20server,family%20of%20services%20in%20the%20Angular%20HTTP%20library.
-  getSong() {
-    var element = document.getElementById('spotifyTest');
-    if (element != null) {
-      //silly button to get the response from spotify to print out, for some reason console.log was giving me problems
-      element.innerHTML = JSON.stringify(
-        this.httpClient.get(this.songUrl, { headers: this.headers })
-      );
-    }
-    //return this.httpClient.request('GET', this.songUrl + '?' + 'name=term', {'headers': this.headers, responseType:'json'});
-    //return this.httpClient.get(this.songUrl).map(this.extractData).catch(this.handleError);
-  }
-
-  //https://developer.spotify.com/documentation/web-api/reference/#/operations/get-the-users-currently-playing-track
-  private extractData(res: Response | any) {
-    let body = res.json();
-    return body.getItem().getName() || {};
-  }
-
-  private handleError(error: Response | any) {
-    // In a real world app, you might use a remote logging infrastructure
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = /*body.error || */ JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    //return Observable.throw(errMsg);
+  
+  updateSong(songId: String) {
+    let url = "https://open.spotify.com/embed/track/"+songId+"?utm_source=generator";
+    document.getElementById("postIframe").src = url;
   }
 }
